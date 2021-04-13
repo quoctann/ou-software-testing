29c690b2aeefbb497fb1d38b950447d71a81cfdb
CosmicFishi <1851050025dung@ou.edu.vn>
2021-04-13 10:16:59 +0700

    Added project folder, adjusted README.md for more informations in configurating project

---
+++ OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/PrimaryController.java
@@ -0,0 +1,12 @@
+package com.ou_software_testing.ou_software_testing;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+}
---
+++ OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.ou_software_testing.ou_software_testing;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
