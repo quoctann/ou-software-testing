f342063b6c76a365d2c09709b52bc5e577608dc9
CosmicFishi <1851050025dung@ou.edu.vn>
2021-04-13 12:42:35 +0700

    added login.fxml

--- OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/LoginController.java
+++ OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/LoginController.java
@@ -3,10 +3,10 @@ package com.ou_software_testing.ou_software_testing;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class PrimaryController {
+public class LoginController {
 
     @FXML
     private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+        App.setRoot("main");
     }
 }
--- OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/MainController.java
+++ OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/MainController.java
@@ -3,10 +3,10 @@ package com.ou_software_testing.ou_software_testing;
 import java.io.IOException;
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class MainController {
 
     @FXML
     private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+        App.setRoot("login");
     }
 }
