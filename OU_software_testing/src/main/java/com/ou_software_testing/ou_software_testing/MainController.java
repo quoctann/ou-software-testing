1d18f1a03192a01789d17e0912b46bf05f9834cb
CosmicFishi <1851050025dung@ou.edu.vn>
2021-04-17 14:01:10 +0700

    Finished sell_menu.fxml, working on other menu

--- OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/MainController.java
+++ OU_software_testing/src/main/java/com/ou_software_testing/ou_software_testing/MainController.java
@@ -11,13 +11,49 @@ public class MainController {
     
     private Stage stage;
     private Scene scene;
+    private String[] name = {
+        "sell_menu", 
+        "statistic_menu", 
+        "products_menu", 
+        "search_menu", 
+        "login",
+        "main"};
     
     @FXML
-    private void switchToLogin(ActionEvent actionEvent) throws IOException {
-        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
-        scene = new Scene(App.loadFXML("login"));
-        stage.setScene(scene);
-        stage.hide();
-        stage.show();
+    private void switchToSellMenu(ActionEvent actionEvent) {
+        switchMenu(actionEvent, name[0]);
+    }
+    @FXML
+    private void switchToStatisticMenu(ActionEvent actionEvent) {
+        switchMenu(actionEvent, name[1]);
+    }
+    @FXML
+    private void switchToProductsMenu(ActionEvent actionEvent) {
+        switchMenu(actionEvent, name[2]);
+    }
+    @FXML
+    private void switchToSearchMenu(ActionEvent actionEvent) {
+        switchMenu(actionEvent, name[3]);
+    }
+    @FXML
+    private void switchToLogin(ActionEvent actionEvent){
+        switchMenu(actionEvent, name[4]);
+    }
+    @FXML
+    private void switchMain(ActionEvent actionEvent) {
+        switchMenu(actionEvent, name[5]);
+    }
+    
+    private void switchMenu(ActionEvent actionEvent, String name) {
+        try {
+            stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            scene = new Scene(App.loadFXML(name));
+            stage.setScene(scene);
+            stage.hide();
+            stage.show();
+        } catch (IOException ex) {
+            System.out.println("Error while switching to" + name);
+            ex.printStackTrace();
+        }
     }
 }
