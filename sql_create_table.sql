6058a1fe8e74ef8817a4f035f3ef80e64fef5a05
Ngô Văn Hậu <43901976+kaneki-sama@users.noreply.github.com>
2021-04-19 18:50:54 +0700

    ADD: add count to product and order_detail table, 2 primary in order_detail is product_id and user_id

--- sql_create_table.sql
+++ sql_create_table.sql
@@ -36,37 +36,41 @@ CREATE TABLE `category` (
 
 LOCK TABLES `category` WRITE;
 /*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'ao','male'),(2,'quan','male'),(3,'ao','female'),(4,'quan','female');
 /*!40000 ALTER TABLE `category` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `order`
+-- Table structure for table `order_detail`
 --
 
-DROP TABLE IF EXISTS `order`;
+DROP TABLE IF EXISTS `order_detail`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `order` (
+CREATE TABLE `order_detail` (
   `product_id` int NOT NULL,
-  `customer_id` int NOT NULL,
+  `user_id` int NOT NULL,
   `day_time` datetime NOT NULL,
   `payment_method` int NOT NULL,
-  PRIMARY KEY (`product_id`,`customer_id`),
-  KEY `fk_customer_id_idx` (`customer_id`),
+  `price` decimal(19,2) DEFAULT NULL,
+  `connt` int DEFAULT '1',
+  PRIMARY KEY (`product_id`,`user_id`),
+  KEY `fk_customer_id_idx` (`user_id`),
   KEY `fk_payment_id_idx` (`payment_method`),
-  CONSTRAINT `fk_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `fk_customer_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
   CONSTRAINT `fk_payment_id` FOREIGN KEY (`payment_method`) REFERENCES `payment_method` (`id`),
   CONSTRAINT `fk_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `order`
+-- Dumping data for table `order_detail`
 --
 
-LOCK TABLES `order` WRITE;
-/*!40000 ALTER TABLE `order` DISABLE KEYS */;
-/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+LOCK TABLES `order_detail` WRITE;
+/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
+INSERT INTO `order_detail` VALUES (1,1,'2020-04-19 00:00:00',1,100.99,1),(2,1,'2020-05-19 00:00:00',2,400.20,1);
+/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -89,6 +93,7 @@ CREATE TABLE `payment_method` (
 
 LOCK TABLES `payment_method` WRITE;
 /*!40000 ALTER TABLE `payment_method` DISABLE KEYS */;
+INSERT INTO `payment_method` VALUES (1,'momo'),(2,'argibank'),(3,'ofline');
 /*!40000 ALTER TABLE `payment_method` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -104,9 +109,10 @@ CREATE TABLE `product` (
   `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `category` int NOT NULL,
   `origin` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `price` decimal(2,0) NOT NULL,
-  `size` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `image` mediumblob NOT NULL,
+  `price` decimal(19,2) NOT NULL,
+  `size` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci DEFAULT NULL,
+  `image` mediumblob,
+  `count` int DEFAULT '1',
   PRIMARY KEY (`id`),
   CONSTRAINT `fk_pro_cate` FOREIGN KEY (`id`) REFERENCES `category` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
@@ -118,6 +124,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'ao den',1,'USA',5000000.00,'M',NULL,2),(2,'ao den',1,'USA',500.00,'XL',NULL,6),(3,'ao den',2,'VN',3.00,'M',NULL,30);
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -146,7 +153,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'Dũng','male','Tp. Ho Chi Minh','123456789','dung@gmail.com','123'),(2,'Hậu','male','Tây Nguyên','12345678','hau@gmail.com','456'),(3,'Alex','female','USA','8465655123','alex5@gmail.com','789'),(4,'Grant','male','India','6515661565','grant.ou@gmail.com','111');
+INSERT INTO `user` VALUES (1,'DÅ©ng','male','Tp. Ho Chi Minh','123456789','dung@gmail.com','123'),(2,'Háº­u','male','TÃ¢y NguyÃªn','12345678','hau@gmail.com','456'),(3,'Alex','female','USA','8465655123','alex5@gmail.com','789'),(4,'Grant','male','India','6515661565','grant.ou@gmail.com','111');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -159,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-04-19 16:18:05
+-- Dump completed on 2021-04-19 18:48:52
