2c18db17c56e63de157b30e41abef84af1fa1c0d
Uyen Nguyen <1851050182uyen@ou.edu.vn>
2021-04-24 13:15:13 +0700

    update test

--- OU_software_testing/src/test/java/LoginTester.java
+++ OU_software_testing/src/test/java/LoginTester.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 
+import com.ou_software_testing.ou_software_testing.GlobalContext;
 import com.ou_software_testing.ou_software_testing.pojo.User;
 import com.ou_software_testing.ou_software_testing.services.JdbcServices;
 import com.ou_software_testing.ou_software_testing.services.UserServices;
 import java.sql.Connection;
 import java.sql.SQLException;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -20,10 +24,14 @@ import org.junit.jupiter.api.Test;
  */
 public class LoginTester {
     
+    Connection conn = JdbcServices.getConnection();
+    
+    
+    
     @Test
-    public void testLogin() throws SQLException{
-        Connection conn = JdbcServices.getConnection();
+    public void testSuccessfull() throws SQLException{
         User user = new UserServices(conn).getUserInfo(null, "123");
-        Assertions.assertTrue(user == null, "lỗi không nhập username hoặc password");
+        Assertions.assertTrue(user != null , "Lỗi sai username hoặc password");
     }
+        
 }
